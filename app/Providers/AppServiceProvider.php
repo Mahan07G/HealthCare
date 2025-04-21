e596b8fe5a76049990a9d24c2669d192ba6b8da7
Mahan07Ge <pmagan205@gmail.com>
2025-04-21 21:54:38 +0530

    Fix HTTPS forcing and clear config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        if (env('APP_ENV') === 'production' || env('FORCE_HTTPS') === true) {
+        // Force HTTPS in production
+        if (env('APP_ENV') === 'production' || env('FORCE_HTTPS') === 'true') {
             URL::forceScheme('https');
         }
     }
